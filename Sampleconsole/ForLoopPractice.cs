171d6b7fe1d89dcba154d307e0eebfe9d9d494ee
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-15 09:09:17 +0530

    Added SumOfMultipleOfEachNumbersWithTwo class

---
+++ Sampleconsole/ForLoopPractice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Write a program while developing new class and generate method new as SumOfMultiplyOfEachNumberwithTwo
+
+*withing the Range on  2 to 5 and return the value*/
+namespace Sampleconsole
+{
+    public class ForLoopPractice
+    {
+        public int SumOfMultiplyOfEachNumberwithTwo(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                sum += i * 2;
+            }
+            return sum;
+        }
+    }
+}
--- Sampleconsole/Program.cs
+++ Sampleconsole/Program.cs
@@ -17,5 +17,9 @@ public class Program
         ForLoopTest forLoopTest = new ForLoopTest();
         int result = forLoopTest.CalculateProduct(1, 5); // Example range from 1 to 5
         Console.WriteLine($"The product of numbers from 1 to 5 is: {result}");
+        //Create an instance of ForLoopPractice and call the method to calculate the sum of multiplied numbers in a range
+        ForLoopPractice forLoopPractice = new ForLoopPractice();
+        int result1 = forLoopPractice.SumOfMultiplyOfEachNumberwithTwo(2, 5); // Example range from 2 to 5
+        Console.WriteLine($"The sum of each number multiplied by 2 from 2 to 5 is: {result1}");
     }
 }
