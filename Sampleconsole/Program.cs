e5ee2fe605ebcd7c1689b853f0aa6de243bcf67d
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-15 08:04:44 +0530

    Developed new class SumOfNumbers

--- Sampleconsole/Program.cs
+++ Sampleconsole/Program.cs
@@ -1,30 +1,11 @@
-﻿/*
-Write a class with name as SumOfMoneyFromTwoFriends 
-Method name SumOfMoney Which Takes Two Integers as input  and
-prints to the Console How much is the Sum of Money from Two Friends
-*/
-//creat a class 
-//class name as SumOfMoneyFromTwoFriends
-//Method name is SumOfMoney
-//Sum(int Frd1, Frd2) 
-//Console.WriteLine("How much is the Sum of Money from Two Friends:");
-using System.Runtime.InteropServices;
+﻿using Sampleconsole;
 
-class SumOfMoneyFromTwoFriends
+public class Program
 {
-    public void SumOfMoney(int Frd1, int Frd2)
+       public static void Main(string[] args)
     {
-        Console.WriteLine(Frd1 + Frd2);
-    }
-}
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("How much is the Sum of Money From Two Friends:");
-        //creating the object
-        //class_name reference_name = new class name();
-        SumOfMoneyFromTwoFriends test_ref = new SumOfMoneyFromTwoFriends();
-        test_ref.SumOfMoney(25, 40);
+        Console.WriteLine("Hello, World!");
+        SumOfNumbers sumOfNumbers = new SumOfNumbers();
+        sumOfNumbers.CanculateSum();
     }
 }
---
+++ Sampleconsole/SumOfNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole
+{
+    public  class SumOfNumbers
+    {
+        public void CanculateSum()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of numbers from 1 to 100 is: {sum}");
+        }
+    }
+}
