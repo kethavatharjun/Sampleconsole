929bced492702924fc3a7e98371cf87187e3bb36
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-15 08:10:32 +0530

    Added ProductOfNumbers class

---
+++ Sampleconsole/ProductOfNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole
+{
+    public  class ProductOfNumbers
+    {
+        public void CanculateProduct()
+        {
+            long product = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                product *= i;
+            }
+            Console.WriteLine($"The product of numbers from 1 to 10 is: {product}");
+        }
+    }
+}
--- Sampleconsole/Program.cs
+++ Sampleconsole/Program.cs
@@ -2,10 +2,15 @@
 
 public class Program
 {
-       public static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         SumOfNumbers sumOfNumbers = new SumOfNumbers();
         sumOfNumbers.CanculateSum();
+
+
+        // Create an instance of ProductOfNumbers and call the method to calculate the product
+        ProductOfNumbers productOfNumbers = new ProductOfNumbers();
+        productOfNumbers.CanculateProduct();
     }
 }
