7a223ccc86ccc46007f180587d6b2984eff01f60
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-16 22:49:58 +0530

    added few new files.

---
+++ Sampleconsole/PrintOddNumbers.cs
@@ -0,0 +1,18 @@
+﻿/*write a program to print the odd number and 
+     * if it is multiple of 3 from the range 2 to 20 
+     * then print "I am Multiple of 3 and value is {num}"*/
+    public class PrintOddNumbers
+    {
+        public int PrintOddNumbersRange(int a, int b)
+        {
+            int c = a;
+            for (int i = 0; i < a; i++)
+            {
+             if(i%2!=0 && i % 3 == 0)
+                {   
+                    Console.WriteLine("I am Multiple of 3 and value is {0}", i);
+                }
+            }
+            return c;
+        }
+    }
--- Sampleconsole/Program.cs
+++ Sampleconsole/Program.cs
@@ -31,16 +31,92 @@ public class Program
         //Console.WriteLine((number % 2 == 0) ? true : false);
         /*Write a C# program to find the sum of all even numbers between 1 
          * and a given number N, using only one for loop..*/
-        Console.Write("Enter a Number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        int sum = 0;
-        for (int i = 1; i <= number; i++)
-        {
-            if (i % 2 == 0)
+        /*Write a program to to showcase the use of switch case
+
+        input as string IsPaymentDone which can be true of false value it can hold.
+
+        if true then print  to Console like "Payment is Done"
+        if false then print -->"Payment is Pending"*/
+        //bool IsPaymentDone;
+        //IsPaymentDone = Convert.ToBoolean(Console.ReadLine()); //false!=null <--true
+        //switch (IsPaymentDone)
+        //{
+        //    case true:
+        //        Console.WriteLine("Payment is done");
+        //        break;
+        //    case false:
+        //        Console.WriteLine("Payment is Pending");
+        //        break;
+        //}
+        /*develop a menu based program using switch case
+         * EG:
+         * switch case shold allow integer as number 
+         * if number is 1 then code related to Addition of Two Number
+         * if number is 2 then Subtraction of Number
+         * 3 then maximum of element
+         * 4 then  code related to the printing of numbers in between the range*/
+            int choice;
+
+            Console.WriteLine(" MENU BASED PROGRAM ");
+            Console.WriteLine("1. Addition of Two Numbers");
+            Console.WriteLine("2. Subtraction of Two Numbers");
+            Console.WriteLine("3. Find Maximum of Two Numbers");
+            Console.WriteLine("4. Print Numbers in a Range");
+            Console.Write("Enter your choice (1-4): ");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
             {
-                sum += i;
+                case 1:
+                Console.WriteLine("Entered choice is: 1.Addition of Two Numbers");
+                    Console.Write("Enter first number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Addition of Two Numbers Is = " + (a + b));
+                    break;
+
+                case 2:
+                Console.WriteLine("Entered choice is: 2.Subtraction of Two Numbers");
+                Console.Write("Enter first number: ");
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number: ");
+                    int d = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Subtraction of two numbers is = " + (c - d));
+                    break;
+
+                case 3:
+                Console.WriteLine("Entered choice is: 3. Find Maximum of Two Numbers");
+                Console.Write("Enter first number: ");
+                    int e = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number: ");
+                    int f = Convert.ToInt32(Console.ReadLine());
+                    if (e > f)
+                        Console.WriteLine("Maximum = " + e);
+                    else if (f > e)
+                        Console.WriteLine("Maximum = " + f);
+                    else
+                        Console.WriteLine("Both are equal");
+                    break;
+
+                case 4:
+                Console.WriteLine("Entered choice is: 4. Print Numbers in a Range");
+                Console.Write("Enter start of range: ");
+                    int start = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter end of range: ");
+                    int end = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Numbers in the given range:");
+                    for (int i = start; i <= end; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.WriteLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice! Please enter a number between 1 and 4.");
+                    break;
             }
-        }
-        Console.WriteLine("Sum of all even numbers between 1 to {0} = {1}", number, sum);
     }
 }
