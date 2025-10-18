91f787a8071a87ef1c2b0c4dacf89bcfe16d4662
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-18 23:57:58 +0530

    Added Program String Range File.

---
+++ Sampleconsole/ForLoop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole
+{
+    public class ForLoop
+    {
+        //access_specifiers = public, private, protect,default
+        //retun type = void, int, string, float.
+        //Method name
+        //parameters
+        // access_specifier return_type Methodname(parameters);
+        public int Sum(int x, int y) 
+        {
+            int sum = x+y;
+            {
+                Console.WriteLine(sum);
+            }
+            return sum;
+           
+        }
+       
+         //drive code  
+    public static void Main(string[] args)
+        {
+           //Console.WriteLine("hello world");
+           // //Object_creation 
+           // //class_name reference_name = new class_name();
+           // ForLoop name = new ForLoop();
+           // //method calling
+           // int result = name.Sum(2, 3);
+            ProgramString programString = new ProgramString();
+            int result1 = programString.ProgramStringRange(11, 16);
+        }
+    }
+}
---
+++ Sampleconsole/ProgramString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole
+{
+    public class ProgramString
+    {
+        public int ProgramStringRange(int start, int end)
+        {
+            string name = "My Name is Arjun";
+            for(int i = start; i < end; i++)
+            {
+                name += " ";
+                Console.Write(name[i]);
+            }
+            return name.Length;
+        }
+    }
+}
