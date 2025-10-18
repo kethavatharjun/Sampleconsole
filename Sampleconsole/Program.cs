5b06ec14713eff0b3276604dbee3ef37d921d96a
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-18 19:30:11 +0530

    Added UnicodeValues.

--- Sampleconsole/Program.cs
+++ Sampleconsole/Program.cs
@@ -73,7 +73,9 @@ public class Program
         //    }
         //    Console.WriteLine();
         //}
-        RangeOfString rangeOfString = new RangeOfString();
-        rangeOfString.RangeOfStringRange();
+        //RangeOfString rangeOfString = new RangeOfString();
+        //rangeOfString.RangeOfStringRange();
+        Unicode unicode = new Unicode();
+         char result5= unicode.UnicodeValues();
     }
 }
---
+++ Sampleconsole/Unicode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole
+{
+    public class Unicode
+    {
+        public char UnicodeValues()
+        {
+            char letter = 'a';
+            for (int i = 65; i <= 90; i++)
+            {
+                letter = (char)i;
+                Console.Write(letter+ " ");
+            }
+            return letter;
+        }
+    }
+}
