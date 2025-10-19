fa1caf7793648bd55f58b7d0e462edd5e247fc0d
kethavatharjun <kethavatharjun0119@gmail.com>
2025-10-19 15:10:55 +0530

    Added Square Pattern File.

---
+++ Sampleconsole/SquarePattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole
+{
+    public class SquarePattern
+    {
+        public void SquarePatternRange()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                for (int j = 0; j <= 4; j++)
+                {
+                    if (i == 0 || i == 4 || j == 0 || j == 4)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                    Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
